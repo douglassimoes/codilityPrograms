f4366062b3a29345778839db72ff745f609e5af0
Douglas Simões Silva <douglassimoesds@gmail.com>
2016-08-09 20:04:49 -0300

    Correct PermCheck

--- Lesson4/PermCheck.js
+++ Lesson4/PermCheck.js
@@ -1,25 +1,23 @@
+// Correctness and Performance 100%
 
 function solution(A) {
     // write your code in JavaScript (Node.js 4.0.0)
     var counterArray = new Array(A.length).fill(0);
-    var sum = 0;
-    
-    
+
+
     for(var i = 0; i < A.length; i++){
         if(A[i] > A.length){
-            return 0;    
+            return 0;
         }
-        if(counterArray[A[i]] === 0){
-        counterArray[A[i]] = 1;
-        sum += 1;
-            }
+        counterArray[A[i]-1] += 1;
+    }
+
+        
+    for(var i = 0; i < counterArray.length; i++){
+        if(counterArray[i] === 0 || counterArray[i] > 1){
+            return 0;
         }
-        
-        console.log(counterArray);
-        
-     if(sum != 0 && sum == A.length-1){
-         return 1;
-        }  else{
-        return 0;
-            }
+    }
+    
+    return 1;
 }
